49aba25eb6be528a34472c73bbf7af8a63c986e7
Tuleuov Niyaz <niaz230898@gmail.com>
2018-04-25 17:40:01 +0300

    pushed http get

--- Alltest.php
+++ Alltest.php
@@ -1,19 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Niyaz
- * Date: 04.04.2018
- * Time: 16:20
- */
-echo '<div style = "color:#64ff2e;">Test case 1, ok! </div><hr>';
-echo '<div style = "color:#64ff2e;">Test case 2, ok! </div><hr>';
-echo '<div style = "color:#650d0b;">Test case 3, not ok! </div><hr>';
-echo '<div style = "color:#64ff2e;">Test case 4, ok! </div><hr>';
-echo '<div style = "color:#64ff2e;">Test case 5, ok! </div><hr>';
-echo '<div style = "color:#64ff2e;">Test case 6, ok! </div><hr>';
-echo '<div style = "color:#64ff2e;">Test case 7, ok! </div><hr>';
-echo '<div style = "color:#64ff2e;">Test case 8, ok! </div><hr>';
-echo '<div style = "color:#64ff2e;">Test case 9, ok! </div><hr>';
-echo '<div style = "color:#64ff2e;">Test case 10, ok! </div><hr>';
+
+ require 'Init.php';
+ $HTTP_GET_VARS.session_cache_limiter()
+
+
+
 
 ?>
